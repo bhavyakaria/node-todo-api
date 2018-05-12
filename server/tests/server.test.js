c2e739f69d07ca74039d6654ecfb7a87369ea6bb
bhavyakaria <bhavyakaria@yahoo.in>
2018-05-12 13:42:52 +0530

    Addind Test Cases for apis.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test.'
 }, {
+    _id: new ObjectID(),
     text: 'Second test.'
 }];
 
@@ -70,4 +73,32 @@ describe('GET /todos', () => {
             })
             .end(done);
     });
+});
+
+describe('GET /todos/:id', () => {
+    it('should get todos by id', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text)
+            })
+            .end(done);
+    });
+
+    it('should return 404 on id not found', (done) => {
+        let hexid = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/5af69e92691ddc23e000d0ad`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 on invalid id', (done) => {
+        request(app)
+            .get(`/todos/123abc`)
+            .expect(404)
+            .end(done);
+    });
+
 });
